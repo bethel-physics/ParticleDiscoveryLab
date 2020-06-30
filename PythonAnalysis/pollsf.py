83d47c8b73fa8ed6a94e8e487b136b3717faddfb
Jack Sisson <65562790+SissonJ@users.noreply.github.com>
2020-06-30 13:37:53 -0500

    created a header

--- PythonAnalysis/pollsf.py
+++ PythonAnalysis/pollsf.py
@@ -1,3 +1,8 @@
+# Created by Jack Sisson on 6/30/2020
+# Email: jsis2000@outlook.com
+# Translation of the matlab function pollsf.m https://github.com/AlejGarcia/NM4P
+
+
 import numpy as np
 import math
 

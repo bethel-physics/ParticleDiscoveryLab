1a51a6968cc8e5f38427091f3726cbfb879b4bbd
Jack Sisson <65562790+SissonJ@users.noreply.github.com>
2020-06-30 13:30:52 -0500

    student version

--- PythonAnalysis/MuonAnalysis_python2020.py
+++ PythonAnalysis/MuonAnalysis_python2020.py
@@ -6,11 +6,10 @@ from scipy.optimize import curve_fit
 from scipy.stats import norm
 import pollsf
 
-#attempt with pickle
-#data = pickle.load(open('DoubleMuParked_100k.pkl','rb'))
-data = pickle.load(open('DoubleMuParked_1M.pkl','rb'))
+# Loading data from the .pkl file
+data = pickle.load(open('DoubleMuParked_100k.pkl','rb'))
 
-# This program begins by loading a Txt file that contains the  
+# This program begins by loading a pkl file that contains the  
 # momentum and energy data from about 1M events. It produces a 
 # graph that has mass on the x-axis and the number of muon pairs 
 # with that mass on the y-axis. 
@@ -19,8 +18,7 @@ data = pickle.load(open('DoubleMuParked_1M.pkl','rb'))
 ## DAY 1 = RECONSTRUCTION
 
 # Choose how many events to process
-#Ntoprocess = input("How many events to process? ")
-Ntoprocess=1000000
+Ntoprocess = input("How many events to process? ")
 Ntoprocess = int(Ntoprocess)
 
 # Initialize a vector that will hold 1 invariant mass per event
@@ -28,38 +26,20 @@ Masses = []
 KineticEnergy = []
 
 # User-chosen min/max values and resolution
-#Min = input("Type in your min (in GeV): ")
-Min=2.8
+Min = input("Type in your min (in GeV): ")
 Min = float(Min)
-#Max = input("Type in your max (in GeV): ")
-Max=3.5
+Max = input("Type in your max (in GeV): ")
 Max = float(Max)
-#n = input("Type in the number of bins: ")
-n=50
+n = input("Type in the number of bins: ")
 n = int(n)
 BinWidth = (Max - Min)/n
-
-E=0
-xMom=0
-M=0
-Px=0
-Py=0
-Pz=0
-xK=0
 
 # Loop over the number of events with at least 2 muons
 print("Looping over ", Ntoprocess, " events...", sep=" ")
 for i in range(Ntoprocess):
+   
     # COMPUTE the mass of particle X -> mu mu
     
-    E = data[i][0] + data[i][1]
-    Px = data[i][2] + data[i][3]
-    Py = data[i][4] + data[i][5]
-    Pz = data[i][6] + data[i][7]
-    M = math.sqrt(E**2 - Px**2 - Py**2 - Pz**2)
-    Masses.append(M.real)
-    
-
 
     # THINK: Is this mass in your window from Min to Max? 
     #        What should you do if it"s outside the window?
@@ -69,10 +49,9 @@ for i in range(Ntoprocess):
     # Calculate the Kinetic Energy of particle X. 
     # Store KE and mass values to plot later
     # Tip: make sure you mass value if "real" by using real(massvalue)
-    if M> Min and M<Max:
-        M=(M).real
-        xK=E-M
-        KineticEnergy.append(xK)
+    
+    
+    
 
 
 #end of the for loop
@@ -90,80 +69,36 @@ for i in range(Ntoprocess):
 
 
 # Draw a HISTOGRAM of counts versus mass
-# HISTOGRAM needs: Mass values list, Mass "bin edges" list 
+# HISTOGRAM needs: a list of values, the number of bins, and the range
+# plt.hist will return the y values of counts, the x values of the bin edges, and a silent list of individual patches
 plt.figure()
-x, bins, patches=plt.hist(x=Masses, bins=n, range=(Min, Max), histtype='step',alpha=.5)
-center = (Max - Min)/n
-centers = []
-for i in range(len(bins)-1):
-    centers.append(bins[i]+center*.5)
-
-err=[[],[]]
-for i in range(len(x)):
-    if x[i]==0:
-        err[1].append(1)
-    else:
-        err[1].append( math.sqrt(x[i]))
-    err[0].append( math.sqrt(x[i]))
-
-plt.errorbar(centers, x, yerr=err, fmt='none', ecolor='red')
-plt.xlabel('GeV') 
-plt.ylabel('Number of muon pairs') 
-  
-plt.title('MuonLab GeV/Counts\n\n', fontweight ="bold") 
-#plt.show()
-
-counts=x
-binEdg=bins
-error=err
-midpoints=centers
 
 
 
 # THINK: What should the ERROR BARS be for each bin? 
 #        What should you do if the bin has ZERO entries?
 # Tools: 
-# HISTCOUNTS: gives counts, needs bin edges + input values (mass)
-# ERRORBAR: draws dots+bars, needs bin centers, y values, down 
-#           uncert list, up uncert list 
+# plt.errorbars: draws dots+bars, needs bin centers, y values, 2d array of down 
+#           uncert list/up uncert list 
 
 
-
+plt.show()
 
 # Draw another HISTOGRAM of counts vs kinetic energy
 # Add ERROR BARS
 plt.figure()
-x, bins, patches=plt.hist(x=KineticEnergy, bins=n, range=(0, 800),histtype='step', log=True,alpha=.5)
-center = (800)/n
-centers=[]
-for i in range(len(bins)-1):
-    centers.append(bins[i]+center*.5)
 
-err=[[],[]]
-for i in range(len(x)):
-    if x[i]==0:
-        err[1].append(1)
-    else:
-        err[1].append( math.sqrt(x[i]))
-    err[0].append( math.sqrt(x[i]))
 
-plt.errorbar(centers, x, yerr=err, fmt='none', ecolor='red')
-plt.xlabel('KE') 
-plt.ylabel('Number of muon pairs') 
-  
-plt.title('MuonLab KE/Counts\n\n', fontweight ="bold") 
-#plt.show()
+plt.show()
 
 
 
 ### Great work! SAVE these plots to represent your RAW DATA.
-###             SAVE a DAY1 workspace (delete large E, px, py, pz)
 
 ## DAY 2 = FITTING -- fit background on either side of the y
-# LOAD your DAY1 workspace
 
 # Vocab: imagine a mass plot with a bump in the middle
-# "y window": region along x-axis under the y
+# "Peak window": region along x-axis under the y
 # "background": smoothly falling slope of random events, 
 #               including some of the events in the y window
 # "signal": events in the y window minus the background
@@ -172,47 +107,20 @@ plt.title('MuonLab KE/Counts\n\n', fontweight ="bold")
 
 
 
-# REMOVE the y window completely from your list of: 
+# REMOVE the peak window completely from your list of: 
 # bin edges, bin centers, counts, uncertainties. 
 # This forms your BACKGROUND dataset
 
 
-bkgCount=[]
-bkgBin=[]
-bkgErr=[[],[]]
-bkgMids=[]
-for i in range(17):
-    bkgCount.append(counts[i])
-    bkgBin.append(binEdg[i])
-    bkgErr[0].append(error[0][i])
-    bkgErr[1].append(error[1][i])
-    bkgMids.append(midpoints[i])
-for i in range(17):
-    bkgCount.append(counts[i+33])
-    bkgBin.append(binEdg[i+33])
-    bkgErr[0].append(error[0][i+33])
-    bkgErr[1].append(error[1][i+33])
-    bkgMids.append(midpoints[i+33])
-    
-bkgBin.append(binEdg[50])
-#print(len(bkgBin))
-#print(bkgMids)
-#print(bkgCount)
-#print("{")
-#for i in range(len(bkgMids)-1):
-#    print("{", bkgMids[i], ",", bkgCount[i],"},", sep=" ")
-#print("{", bkgMids[44], ",", bkgCount[44],"} }", sep=" ")
 
 
 # PERFORM a polynominal fit to the background
 # THINK: Which type of curve do you expect will match your data best?
 #        Imagine a curve connecting the two sides under your y.
 # Tool: POLLSF gives fit params, uncerts, y-values, chi^2 value
-#       needs bin centers, counts, uncertainties, N params
+#       needs bin centers, counts, up uncertainties, N params
 #       This is a least-squares fitter that uses uncerts!
-m=2
-fitparams, uncerts, yvalues, chisq = pollsf.pollsf(bkgMids, bkgCount, bkgErr[1], m)
-print(chisq/(50-m))
+
 
 
 # EVALUATE your fit by chi^2 and plotting
@@ -223,27 +131,8 @@ print(chisq/(50-m))
 #    OPTIMALLY, Chi^2 / (# points - # parameters) is around 1
 # REPEAT fitting until you are satisfied
 plt.figure()
-x, bins, patches=plt.hist(x=Masses, bins=n, range=(Min, Max), histtype='step',alpha=.5)
-center = (Max - Min)/n
-centers = []
-for i in range(len(bins)-1):
-    centers.append(bins[i]+center*.5)
 
-err=[[],[]]
-for i in range(len(x)):
-    if x[i]==0:
-        err[1].append(1)
-    else:
-        err[1].append( math.sqrt(x[i]))
-    err[0].append( math.sqrt(x[i]))
-
-plt.errorbar(centers, x, yerr=err, fmt='none', ecolor='red')
-plt.xlabel('GeV') 
-plt.ylabel('Number of muon pairs') 
-  
-plt.title('Fit Function\n\n', fontweight ="bold") 
-plt.plot(bkgMids,yvalues)
-#plt.show()
+plt.show()
 
 
 
@@ -252,38 +141,16 @@ plt.plot(bkgMids,yvalues)
 #        What do you expect the curve to look like after bkg subtraction?
 
 # CALCULATE background = yourFit(bin center) for all bins
-noBkg=np.zeros(len(midpoints))
-
-for j in range(m):
-    for i in range(len(midpoints)):
-        noBkg[i]=noBkg[i]+(midpoints[i]**(j))*fitparams[j]
-for i in range(len(midpoints)):
-    noBkg[i]=abs(counts[i]-noBkg[i])
         
 
-plt.figure()
-plt.step(x=midpoints, y=noBkg ,where='mid', alpha=.3 )
- 
-err=[[],[]]
-for i in range(len(noBkg)):
-    if math.sqrt(noBkg[i])<1:
-        err[1].append(1)
-    else:
-        err[1].append( math.sqrt(noBkg[i]))
-    err[0].append( math.sqrt(noBkg[i]))
 
-plt.errorbar(midpoints, noBkg, yerr=err, fmt='none', ecolor='red')
-plt.xlabel('GeV') 
-plt.ylabel('Number of muon pairs') 
-
-  
-plt.title('Signal\n\n', fontweight ="bold") 
-#plt.show()
 
 
 # PLOT the background curve on top of your mass histogram (save it!)
 # THINK: Are your estimated bkg values at all uncertain? 
-##figure()
+plt.figure()
+
+plt.show()
 
 
 # EVALUATE signal = data - background
@@ -294,7 +161,9 @@ plt.title('Signal\n\n', fontweight ="bold")
 
 
 # PLOT the signal-only y with ERROR BARS
-##figure()
+plt.figure()
+
+plt.show()
 
 
 # Great work! Save the data+background and signal-only plots as ANALYSIS
@@ -305,30 +174,11 @@ plt.title('Signal\n\n', fontweight ="bold")
 
 # EXTRACT the characteristics (mean, width, uncerts) of your signal y
 # THINK: Which statistical distribution describes your signal y?
-# TOOL: Curve-fitting app has a GUI to walk you through fitting, with 
-#       automatic plotting and quality prints! Select x and y data and the 
-#       function you"d like. (use Fit -> Save to Workspace) 
+# TOOL: curve_fit is a function that takes in the function you want to fit, x and y data,
+#       and an array in the form p0=[max(y),mean,uncerts]
 # SAVE a screencapture of your fit and its parameters.
-y = np.delete(noBkg,[33,47])
-y = np.delete(y,[0,16])
-x = np.delete(midpoints,[33,47])
-x = np.delete(x,[0,16])
+plt.figure()
 
-
-# weighted arithmetic mean (corrected - check the section below)
-mean = sum(x * y) / sum(y)
-sigma = np.sqrt(sum(y * (x - mean)**2) / sum(y))
-
-def Gaus(x,a,x0,sigma):
-    return a*np.exp(-(x-x0)**2/(2*sigma**2))
-
-popt,pcov = curve_fit(Gaus,x,y,p0=[max(y),mean,sigma])
-print(popt)
-perr = np.sqrt(np.diag(pcov))
-print(perr)
-x=np.linspace(30,150,501).tolist()
-
-plt.plot(x,Gaus(x,*popt),'r',label='fit')
 plt.show()
 
 # COMPARE: NSignal in signal y to NBackground under the y region
